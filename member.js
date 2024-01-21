22b4cd82819bba108c8132093649e8a14f64c0b2
Bibek Timilsina <154731370+bibektimilsina00@users.noreply.github.com>
2024-01-21 06:26:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        scope: {
+            member: '='
+        },
+        templateUrl: 'app/components/skills/member.html'
+        
+    };
+}
